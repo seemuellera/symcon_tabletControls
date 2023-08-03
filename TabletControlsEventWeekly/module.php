897d4cd85d61b7eedfa0ff02941402cc27c8776f
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-08-03 21:35:46 +0200

    reference cleanups

--- TabletControlsEventWeekly/module.php
+++ TabletControlsEventWeekly/module.php
@@ -81,6 +81,20 @@ class TabletControlsEventWeekly extends IPSModule {
 		
 		$newInterval = $this->ReadPropertyInteger("RefreshInterval") * 1000;
 		$this->SetTimerInterval("RefreshInformation", $newInterval);
+
+		// Clean old references
+		$referenceList = $this->GetReferenceList();
+		foreach ($referenceList as $currentReference) {
+
+			$this->UnregisterReference($this->InstanceID, $currentReference);
+		}
+
+		// Clean old message registration
+		$messagesList = $this->GetMessageList();
+		foreach ($messagesList as $currentMessage) {
+
+			$this->UnregisterMessage($this->InstanceID, $currentMessage);
+		}
 				
 		// Diese Zeile nicht löschen
 		parent::ApplyChanges();
